cd248ff9c3a689853330728f9473f0c7fae6dd98
juanmarinom <juanmarinom@gmail.com>
2020-05-03 19:32:28 +0200

    Herramientas control robot Vrep

--- SIMULACION_Vrep/PROGRAMACION_Vrep/PruebaProgVrep/PruebaProg1.py
+++ SIMULACION_Vrep/PROGRAMACION_Vrep/PruebaProgVrep/PruebaProg1.py
@@ -1,24 +1,38 @@
 import vrep
 import time
 import math
-
+import DeltaJardinero
 # Vrep connection
 vrep.simxFinish(-1) # Ending of previous communication open
-clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
-
+clientID = vrep.simxStart('127.0.0.1', 3999, True, True, 5000, 5)
+vrep.simxSynchronous(clientID, True)
 
 if clientID != -1:
     print("Connection succeed")
-    returnCode, handle = vrep.simxGetObjectHandle(clientID, 'Prismatic_joint', vrep.simx_opmode_oneshot_wait)
-    returnCode2, handle2 = vrep.simxGetObjectHandle(clientID, 'div_join_1', vrep.simx_opmode_oneshot_wait)
-    print(handle)
-    print(handle2)
-    #actualPos = vrep.simxGetJointPosition(clientID, handle2,vrep.simx_opmode_oneshot)
-    #print(actualPos)
-   actualPos = vrep.simxSetJointPosition(clientID, handle2,0.5, vrep.simx_opmode_oneshot)
-   print(actualPos)
+    Robot = DeltaJardinero.Delta('div_join_1', 'div_join_2', 'div_join_3',1, -1, 1, 24, 24, 24, clientID)
+    # Set initial angles
+    #r, handle1 = vrep.simxGetObjectHandle(clientID, 'div_join_1', vrep.simx_opmode_oneshot_wait)
+    #print(handle1)
+    #vrep.simxSynchronous(clientID, True)
+    #Robot.setVelocity(0.5, 0.5, 0.5)
+
+    #Posicion inicial
+
+    Robot.setAngles(40,10,10)
+    time.sleep(2)
+    #vrep.simxSynchronousTrigger(clientID)
 
 
+
+    #ret = vrep.simxSetObjectFloatParameter(clientID, handle1, vrep.sim_jointfloatparam_upper_limit, velocity, vrep.simx_opmode_oneshot)
+    #time.sleep(0.5)
+    #vrep.simxSetJointTargetPosition(clientID, handle1, angle * math.pi / 180, vrep.simx_opmode_oneshot)
+    #time.sleep(1)
+
+    #vrep.simxSynchronousTrigger(clientID)
+    #time.sleep(1)
+    #print(POS)
+    #time.sleep(1)
 
 else:
     print("Failed connection")
