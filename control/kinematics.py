c97bad2ac14556782b7382be007be0756bb33618
Palmero96 <alvaropalmero@hotmail.com>
2020-05-04 21:00:53 +0200

    commit

--- control/kinematics.py
+++ control/kinematics.py
@@ -15,43 +15,33 @@ def forward_kin(theta):
     # theta = [10,0,0]
 
     # Se calculan los puntos pi y zi
-    p = [0,0,0]
-    z = [0,0,0]
-    c = [0,0,0]
-    h = [0,0,0]
+    q = [0,0,0]
+    r = [0,0,0]
+    s = [0,0,0]
+    t = [0,0,0]
+
+    d = (b-a)*sqrt(3)/6
 
     index=0
     for alph,th in zip(alpha,theta):
-        p[index] = np.array(
-            [sqrt(3)/6*a*math.cos(math.radians(alph)),
-             sqrt(3)/6*a*math.sin(math.radians(alph)),
-             0])
+        q[index] = 2*math.cos(math.radians(alph))*(d-l1*math.sin(math.radians(th)))
+        r[index] = 2*math.sin(math.radians(alph))*(d-l1*math.sin(math.radians(th)))
+        s[index] = 2*math.cos(math.radians(th))*l1
+        t[index] = -2*math.sin(math.radians(th))*d*l1
 
-        z[index] = p[index] + np.array(
-            [l1*math.sin(math.radians(th))*math.cos(math.radians(alph)),
-             l1*math.sin(math.radians(th))*math.sin(math.radians(alph)),
-             l1*math.cos(math.radians(th))])
-
-        # A c hay que sumarle las incognitas x', y' y z'
-        c[index] = np.array(
-            [sqrt(3)/6*b*math.cos(math.radians(alph)),
-             sqrt(3)/6*b*math.sin(math.radians(alph)),
-             0])
-
-        # H serán los vectores auxiliares para la generación del sistema de ecuaciones
-        h[index] = c[index] - z[index]
-
-        print("El punto p_{} = {}".format(index+1, p[index]))
-        print("El punto z_{} = {}".format(index+1, z[index]))
-        print("El punto c_{} = {}".format(index+1, c[index]))
+        # print("El punto q_{} = {}".format(index+1, q[index]))
+        # print("El punto r_{} = {}".format(index+1, r[index]))
+        # print("El punto s_{} = {}".format(index+1, s[index]))
+        # print("El punto t_{} = {}".format(index+1, t[index]))
 
         index+=1
 
-    # matrix_gauss = [[2*h[0][0]-2*h[1][0], 2*h[0][1]-2*h[1][1], 2*h[0][2]-2*h[1][2], h[0][0]**2+h[0][1]**2+h[0][2]**2-h[1][0]**2-h[1][1]**2-h[1][2]**2],
-    #      [2*h[0][0]-2*h[2][0], 2*h[0][1]-2*h[2][1], 2*h[0][2]-2*h[2][2], h[0][0]**2+h[0][1]**2+h[0][2]**2-h[2][0]**2-h[2][1]**2-h[2][2]**2],
-    #      [2*h[2][0]-2*h[1][0], 2*h[2][1]-2*h[1][1], 2*h[2][2]-2*h[1][2], h[2][0]**2+h[2][1]**2+h[2][2]**2-h[1][0]**2-h[1][1]**2-h[1][2]**2]]
-    ecu = [[2*h[0][0]-2*h[1][0], 2*h[0][1]-2*h[1][1], 2*h[0][2]-2*h[1][2], h[0][0]**2+h[0][1]**2+h[0][2]**2-h[1][0]**2-h[1][1]**2-h[1][2]**2],
-         [2*h[0][0]-2*h[2][0], 2*h[0][1]-2*h[2][1], 2*h[0][2]-2*h[2][2], h[0][0]**2+h[0][1]**2+h[0][2]**2-h[2][0]**2-h[2][1]**2-h[2][2]**2]]
+    ecu = [[q[0]-q[1],r[0]-r[1],s[0]-s[1],t[0]-t[1]],[q[0]-q[2],r[0]-r[2],s[0]-s[2],t[0]-t[2]]]
+
+    print(q)
+    print(r)
+    print(s)
+    print(t)
 
     for ec in ecu:
         print(ec)
@@ -59,13 +49,13 @@ def forward_kin(theta):
     # Se define como lambda z' y se obtienen las relaciones respecto a lambda de x' e y'
     # en un vector de (real, lambda)
     z = [0,1]
-    y = [((-ecu[1][0]/ecu[0][0])*(-ecu[0][3])+ecu[1][3])*(1/((ecu[1][0]/ecu[0][0])*(-ecu[0][1])+ecu[1][1])),
-         ((-ecu[1][0]/ecu[0][0])*(-ecu[0][2])+ecu[1][2])*(1/((ecu[1][0]/ecu[0][0])*(-ecu[0][1])+ecu[1][1]))]
-    x = [(-ecu[0][1]*y[0]-ecu[0][3])/ecu[0][0],(-ecu[0][1]*y[1]-ecu[0][2])/ecu[0][0]]
+    x = [(-(t[0]-t[2])-((r[0]-r[2])*(-(t[0]-t[1])))/(r[0]-r[1]))/((q[0]-q[2])-((r[0]-r[2])*(q[0]-q[1])/(r[0]-r[1]))),
+         (-(s[0]-s[2])-((r[0]-r[2])*(-(s[0]-s[1])))/(r[0]-r[1]))/((q[0]-q[2])-((r[0]-r[2])*(q[0]-q[1])/(r[0]-r[1])))]
+    y = [(-(t[0]-t[1])-x[0]*(q[0]-q[1]))/(r[0]-r[1]),(-(s[0]-s[1])-x[1]*(q[0]-q[1]))/(r[0]-r[1])]
 
     a_ecu = x[1]**2 + y[1]**2 + z[1]**2
-    b_ecu = 2*x[0]*x[1] + 2*h[0][0]*x[1] + 2*y[0]*y[1] + 2*h[0][1]*y[1] + 2*z[0]*z[1] + 2*h[0][2]*x[1]
-    c_ecu = h[0][0]**2 + x[0]**2 + 2*h[0][0]*x[0] + h[0][1]**2 + y[0]**2 + 2*h[0][1]*y[0] + h[0][2]**2 + z[0]**2 + 2*h[0][2]*z[0] - l2**2
+    b_ecu = 2*x[0]*x[1] + 2*y[0]*y[1] + 2*z[0]*z[1] + q[0]*x[1] + r[0]*y[1] + s[0]*z[1]
+    c_ecu = l1**2 + d**2 + x[0]**2 + y[0]**2 + z[0]**2 - l2**2 + q[0]*x[0] + r[0]*y[0] + s[0]*z[0] + t[0]
 
     print("Parametros x,y,z")
     print(x)
@@ -77,12 +67,15 @@ def forward_kin(theta):
     print(b_ecu)
     print(c_ecu)
 
-    sol_z1 = (-b_ecu+sqrt(b_ecu**2-4*a_ecu*c_ecu))/(2*a_ecu)
-    sol_z2 = (-b_ecu-sqrt(b_ecu**2-4*a_ecu*c_ecu))/(2*a_ecu)
+    sol_z1 = (-b_ecu+sqrt(b_ecu**2-4*a_ecu*c_ecu))/(-2*a_ecu)
+    sol_z2 = (-b_ecu-sqrt(b_ecu**2-4*a_ecu*c_ecu))/(-2*a_ecu)
+
+    print(sol_z1)
+    print(sol_z2)
 
     sol_z = max(sol_z1,sol_z2)
-    sol_x = x[0]+sol_z*x[1]
-    sol_y = y[0]+sol_z*y[1]
+    sol_x = x[0]-sol_z*x[1]
+    sol_y = y[0]-sol_z*y[1]
 
     print("Las soluciones son")
     print(round(sol_x,4))
@@ -141,7 +134,8 @@ def inverse_kin(pos):
     return sol
 
 print("\n    INVERSE KINEMATICS     \n")
-pos = inverse_kin([0,0,20])
+pos = inverse_kin([6.977,-3.66,32.487])
 print(pos)
 print("\n    FORWARD KINEMATICS     \n")
+# forward_kin([90,90,90])
 forward_kin(pos)
