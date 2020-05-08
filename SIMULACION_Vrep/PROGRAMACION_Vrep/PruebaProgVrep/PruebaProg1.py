ddb5418702bbc4f017b6055d8fc31088f1bbe546
juanmarinom <juanmarinom@gmail.com>
2020-05-08 18:36:50 +0200

    Herramientas de obtención de la posición del extremo

--- SIMULACION_Vrep/PROGRAMACION_Vrep/PruebaProgVrep/PruebaProg1.py
+++ SIMULACION_Vrep/PROGRAMACION_Vrep/PruebaProgVrep/PruebaProg1.py
@@ -10,29 +10,17 @@ vrep.simxSynchronous(clientID, True)
 if clientID != -1:
     print("Connection succeed")
     Robot = DeltaJardinero.Delta('div_join_1', 'div_join_2', 'div_join_3',1, -1, 1, 24, 24, 24, clientID)
-    # Set initial angles
-    #r, handle1 = vrep.simxGetObjectHandle(clientID, 'div_join_1', vrep.simx_opmode_oneshot_wait)
-    #print(handle1)
-    #vrep.simxSynchronous(clientID, True)
-    #Robot.setVelocity(0.5, 0.5, 0.5)
+
+
 
     #Posicion inicial
 
-    Robot.setAngles(40,10,10)
+    Robot.setAngles(30,-10,-20)
     time.sleep(2)
-    #vrep.simxSynchronousTrigger(clientID)
 
-
-
-    #ret = vrep.simxSetObjectFloatParameter(clientID, handle1, vrep.sim_jointfloatparam_upper_limit, velocity, vrep.simx_opmode_oneshot)
-    #time.sleep(0.5)
-    #vrep.simxSetJointTargetPosition(clientID, handle1, angle * math.pi / 180, vrep.simx_opmode_oneshot)
-    #time.sleep(1)
-
-    #vrep.simxSynchronousTrigger(clientID)
-    #time.sleep(1)
-    #print(POS)
-    #time.sleep(1)
+    #Obtención posición base camara respecto centro robot(centro, parte superior)
+    pos = Robot.getCameraPosition()
+    print(pos)
 
 else:
     print("Failed connection")
